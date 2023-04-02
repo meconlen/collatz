d36eb8b885e4a5574537968dbb87cdf05290f128
Mike Conlen <mike@conlen.org>
2023-04-02 13:55:51 +0000

    check program running with
    meconlen@ctci:~/src/collatz/src$ time ./check 10000000
    
    real    0m17.698s
    user    0m16.562s
    sys     0m1.131s

--- src/check.cpp
+++ src/check.cpp
@@ -1,6 +1,15 @@
+#include <cstdint>
+#include <iostream>
+
 #include "collatz.hpp"
 
 int main(int argc, char* argv[])
 {
+   if(argc != 2) {
+      std::cerr << argv[0] << " n" << std::endl;
+      return -1;
+   }
+   uint64_t n = std::stoull(argv[1]);
+   verify(n);
    return 0;
 }
