6c75c7865c315f788996427952cf89c567414fb3
Mike Conlen <mike@conlen.org>
2023-04-02 14:15:32 +0000

    optimized with unordered map
    meconlen@ctci:~/src/collatz/src$ time ./check 10000000
    
    real    0m10.981s
    user    0m10.081s
    sys     0m0.896s

--- src/collatz.cpp
+++ src/collatz.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
 #include <iostream>
+#include <list>
 #include <map>
+#include <unordered_map>
+#include <utility>
 
 #include "collatz.hpp"
 
@@ -9,20 +12,29 @@ uint64_t collatz(uint64_t value)
    uint64_t n = value;
    if(n == 0) { return 0; }
 
-   static std::map<uint64_t, uint64_t> collatz_memo{ {1, 1} };
+   static std::unordered_map<uint64_t, uint64_t> collatz_memo{ {1, 1} };
    if(collatz_memo.find(n) != collatz_memo.end()) {
       return collatz_memo[n];
    }
    uint64_t count = 1;
+   // list of <n, count> 
+   // so that we can push all the values we learn when we memoize
+   // the counts are forward counts so we push <n, 1>, ...
+
+   std::list<std::pair<uint64_t, uint64_t>> counts;
    while(n != 1) {
+      counts.push_back(std::make_pair(n, count));
       if(n%2 == 0) { 
          n = n/2; 
       } else {
          n = (3*n + 1);
       }
       if(collatz_memo.find(n) != collatz_memo.end()) {
-         collatz_memo[value] = collatz_memo[n] + count;
-         return collatz_memo[n] + count;
+         for(const auto& n_count : counts) {
+            // the n_count is a forward count, so we need to adjust the current count by k-1
+            collatz_memo[n_count.first] = collatz_memo[n] + count - (n_count.second - 1);
+         }
+         return collatz_memo[value];
       }
       ++count;
    }
